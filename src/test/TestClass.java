fe80b6006361d2278a00d3c6538677c19f705bd4
luoyexiaohe <2653473896@qq.com>
2019-03-27 12:15:48 +0800

    最近的测试代码

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -1,9 +1,13 @@
 package test;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 import org.junit.Test;
 
@@ -110,4 +114,100 @@ public class TestClass {
 		String str = rb.getString("path");
 		System.out.println(str);
 	}
+	
+	/**
+	 * 查看阅读集合源码-Collection(Vector)
+	 * @author: JOHN
+	 * @date: 2019年3月25日
+	 * @time: 下午1:30:36
+	 */
+	@Test
+	public void testFunc12() {
+		Collection vector = new Vector<>();
+		vector.add("hello world!");
+	}
+	
+	/**
+	 * 有意思的代码（居然还有这种奇葩操作！！！！）
+	 * @author: JOHN
+	 * @date: 2019年3月25日
+	 * @time: 下午2:06:53
+	 */
+	@Test
+	public void testFunc13() {
+		Map map = new HashMap();
+		map.put("map", map);
+		System.out.println(map.size());
+	}
+	
+	/**
+	 * 测试char数据类型是否支持直接加减操作
+	 * @author: JOHN
+	 * @date: 2019年3月25日
+	 * @time: 下午2:14:49
+	 */
+	@Test
+	public void testFunc14() {
+		char c = 97;
+		System.out.println(c);
+	}
+	
+	/**
+	 * 测试JAVA8的语法糖
+	 * @author: JOHN
+	 * @date: 2019年3月25日
+	 * @time: 下午3:31:01
+	 * 结果：还是没搞懂到底语法糖怎么用，holy shit！
+	 */
+	@Test
+	public void testFunc15() {
+		String[] atp = {"Rafael Nadal", "Novak Djokovic",  
+			       "Stanislas Wawrinka",  
+			       "David Ferrer","Roger Federer",  
+			       "Andy Murray","Tomas Berdych",  
+			       "Juan Martin Del Potro"};  
+			List<String> players =  Arrays.asList(atp);  
+			  
+			// 以前的循环方式  
+			for (String player : players) {  
+			     System.out.println(player + "; ");  
+			}  
+			  
+			// 使用 lambda 表达式以及函数操作(functional operation)  
+			players.forEach((player) -> System.out.println(player + "; "));  
+			   
+			// 在 Java 8 中使用双冒号操作符(double colon operator)  
+			players.forEach(System.out::println);
+	}
+	
+	/**
+	 * 查看hashMap的原理和底层结构
+	 * @author: JOHN
+	 * @date: 2019年3月26日
+	 * @time: 下午10:19:18
+	 */
+	@Test
+	public void testFunc16() {
+		HashMap<String ,Object> map = new HashMap<String ,Object>();
+		map.put("123", 123);
+		map.get("");
+		map.clear();
+	}
+	
+	/**
+	 * 字符串对象测试
+	 * @author: JOHN
+	 * @date: 2019年3月27日
+	 * @time: 上午12:07:07
+	 */
+	@Test
+	public void testFunc17() {
+		String a = "123";
+		String b = "123";
+		String c = new String("123");
+		String d = "12"+"3";
+		System.out.println(b==d);
+		System.out.println(c==d);
+	}
+	
 }
