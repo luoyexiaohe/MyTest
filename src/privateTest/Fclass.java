60652bced04d841498f7cfb324a2b078afe87b5a
luoyexiaohe <2653473896@qq.com>
2019-04-08 09:50:28 +0800

    private关键字练习

---
+++ src/privateTest/Fclass.java
@@ -0,0 +1,13 @@
+package privateTest;
+
+public class Fclass {
+	protected String str = "123";
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+}
---
+++ src/privateTest/Sclass.java
@@ -0,0 +1,8 @@
+package privateTest;
+
+public class Sclass extends Fclass{
+	public static void main(String[] args) {
+		Sclass sc = new Sclass();
+		System.out.println(sc.str);
+	}
+}
